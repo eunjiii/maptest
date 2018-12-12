276b5fdbdf3714b5da84374809202589c5701193
eunjiii <a01049470155@gmail.com>
2018-12-12 18:08:47 +0900

    maptest

---
+++ src/main/java/org/injae/mapper/SampleMapper.java
@@ -0,0 +1,16 @@
+package org.injae.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.injae.domain.StoreVO;
+
+public interface SampleMapper {
+	
+	@Select("select * from tbl_mapex")
+	public List<StoreVO> getList();
+
+	@Select("select * from tbl_mapex2")
+	public List<StoreVO> getList2();
+}
+
